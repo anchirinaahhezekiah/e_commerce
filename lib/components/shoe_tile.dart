9c202c98f41e6dfea8a0083f42c8f68678fe9791
Anchirinaah Hezekiah <ce-hanchirinaah3821@st.umat.edu.gh>
2025-01-26 12:47:05 +0000

    Flutter E-commerce

--- lib/components/shoe_tile.dart
+++ lib/components/shoe_tile.dart
@@ -16,77 +16,81 @@ class ShoeTile extends StatelessWidget {
         color: Colors.grey[100],
         borderRadius: BorderRadius.circular(12),
       ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          //shoe pic
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: Image.asset(shoe.imagePath),
-          ),
-
-          // description
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 25.0),
-            child: Text(
-              shoe.description,
-              style: TextStyle(color: Colors.grey[600]),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            //shoe pic
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: Image.asset(
+                shoe.imagePath,
+              ),
             ),
-          ),
 
-          //price + details
-          Padding(
-            padding: const EdgeInsets.only(left: 25.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    //shoe name
-                    Text(
-                      shoe.name,
-                      style: const TextStyle(
-                          fontSize: 20, fontWeight: FontWeight.bold),
-                    ),
+            // description
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Text(
+                shoe.description,
+                style: TextStyle(color: Colors.grey[600]),
+              ),
+            ),
 
-                    const SizedBox(
-                      height: 5,
-                    ),
-
-                    //price
-                    Text(
-                      '\$${shoe.price}',
-                      style: const TextStyle(
-                        color: Colors.grey,
+            //price + details
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      //shoe name
+                      Text(
+                        shoe.name,
+                        style: const TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
                       ),
-                    ),
-                  ],
-                ),
 
-                //plus bottom
-                GestureDetector(
-                  onTap: onTap,
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: const BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(12),
-                        bottomRight: Radius.circular(12),
+                      const SizedBox(
+                        height: 5,
                       ),
-                    ),
-                    child: const Icon(
-                      Icons.add,
-                      color: Colors.white,
+
+                      //price
+                      Text(
+                        '\$${shoe.price}',
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  ),
+
+                  //plus bottom
+                  GestureDetector(
+                    onTap: onTap,
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomRight: Radius.circular(12),
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          )
-        ],
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
